5a0ec34488a73b623c89e03eb0b467199207f6f6
Mino <mino.rakoto94@gmail.com>
2018-05-01 12:02:29 +0300

    Elah ry tino ah 4

--- db/migrate/20180430135333_directorx.rb
+++
@@ -1,6 +0,0 @@
-class Directorx < ActiveRecord::Migration[5.2]
-  def change
-    #column
-  add_column :movies, :Directorx,  :string
-  end
-end
--- db/migrate/20180501085707_add_column_director_id.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnDirectorId < ActiveRecord::Migration[5.2]
-  def change
-    add_column :movies, :director_id , :integer
-  end
-end
