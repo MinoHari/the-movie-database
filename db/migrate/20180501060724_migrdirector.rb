0eeb9fc69fe9164ec337b2577673675025caeff7
Mino <mino.rakoto94@gmail.com>
2018-05-01 11:55:46 +0300

    Elah ry tino ah 1

--- db/migrate/20180501060724_migrdirector.rb
+++
@@ -1,8 +0,0 @@
-class Migrdirector < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :movies, :director
-    # remove_column :movies, :Director_x
-    add_column :movies, :director_id , :integer
-    #lol
-  end
-end
